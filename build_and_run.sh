9a0c141d85013c058ea79b0bd665d05c6435415a
Felix Kollmann <mail@fkollmann.de>
2022-08-01 20:32:34 +0200

    Change build and run script to provide easy debug mode option

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,4 +4,6 @@ set -e
 
 DOCKER_BUILDKIT=1 docker build -t graphvizget387465 .
 
-docker run -it -p 8080:8080 graphvizget387465
+DEBUG=0
+
+docker run -it -p 8080:8080 -e "DEBUG=$DEBUG" graphvizget387465
